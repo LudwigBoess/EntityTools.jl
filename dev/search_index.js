476c9fde1da6f60cb560a580c7e3b01e20ee29b5
Documenter.jl <documenter@juliadocs.github.io>
2025-04-17 16:32:46 +0000

    build based on 1904f4a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = EntityTools","category":"page"},{"location":"#EntityTools","page":"Home","title":"EntityTools","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for EntityTools.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [EntityTools]","category":"page"}]
+}
