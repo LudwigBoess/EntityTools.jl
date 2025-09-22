af5c1734ad70b5361989118c61a790edaa66f64c
LudwigBoess <ludwig.boess@outlook.de>
2025-09-21 19:19:18 -0500

    streamlining, comments and cleanup

--- src/IO/adios/adios.jl
+++ src/IO/adios/adios.jl
@@ -1,14 +1,37 @@
 abstract type DataInfo end
 
+
+"""
+    struct FieldInfo <: DataInfo
+        fields::Vector{String}
+    end
+
+Struct to store info on field output.
+"""
 struct FieldInfo <: DataInfo
     fields::Vector{String}
 end
 
+"""
+    struct ParticleInfo <: DataInfo
+        species::Vector{Int64}
+        properties::Vector{String}
+    end
+
+Struct to store info on particle output.
+"""
 struct ParticleInfo <: DataInfo
     species::Vector{Int64}
     properties::Vector{String}
 end
 
+"""
+    struct SpectrumInfo <: DataInfo
+        species::Vector{Int64}
+    end
+
+Struct to store info on output spectra.
+"""
 struct SpectrumInfo <: DataInfo
     species::Vector{Int64}
 end
@@ -24,7 +47,7 @@ end
         spectra::Union{SpectrumInfo, Nothing}
     end
 
-
+Struct to store metadata of an Entity simulation.
 """
 struct EntityData 
     path::String
@@ -36,6 +59,13 @@ struct EntityData
     spectra::Union{SpectrumInfo, Nothing}
 end
 
+
+"""
+    read_times(sim_path, subfolder, read_steps=false)
+
+Finds all available output times for a given `subfolder`.
+If `read_steps=true` is set it also returns an array of the output steps constructed from file names.
+"""
 function read_times(sim_path, subfolder, read_steps=false)
     # find all available files in the folder
     files = readdir(joinpath(sim_path, subfolder))
@@ -64,7 +94,8 @@ end
 """
     find_idxs(run_info, i_step, t, i, reduction_function, verbose)
 
-
+Find requested output file(s) based on single/array of steps `i_step`, time `t` or file number `i`.
+If a `reduction_function` is provided, options are interpreted as arrays.
 """
 function find_idxs(run_info, i_step, t, i, reduction_function, verbose)
 
@@ -96,7 +127,7 @@ function find_idxs(run_info, i_step, t, i, reduction_function, verbose)
         if (!isnothing(i_step) && length(i_step) > 1) || 
             (!isnothing(t) &&  length(t) > 1) || 
             (!isnothing(i) && length(i) > 1)
-            error("You can only read one file in this mode. To get the properties over multiple files please provide a reduction_function.")
+            error("You can only read one file in this mode. To get the properties over multiple files please provide a `reduction_function``.")
         end
 
         if !isnothing(i_step)
@@ -121,12 +152,36 @@ function find_idxs(run_info, i_step, t, i, reduction_function, verbose)
 
 end
 
-function read_field_info(sim_path; times=nothing, i_steps=nothing)
+"""
+    select_first_file(sim_path, subfolder)
 
-    times, i_steps = read_times(sim_path, "fields", true)
+Selects the first available file in a `subfolder` or throws an error 
+"""
+function select_first_file(sim_path, subfolder)
 
-    file = adios_open_serial(joinpath(sim_path, "fields", "fields.$(@sprintf("%08i", i_steps[1])).bp"), mode_readRandomAccess)
+    # check if there are files present
+    folder_files = readdir(joinpath(sim_path, subfolder))
+    if isempty(folder_files)
+        error("No files present in $(subfolder)!")
+    end
+    # open first available file
+    file = adios_open_serial(joinpath(sim_path, subfolder, folder_files[1]), mode_readRandomAccess)
+
+    return file
+end
+
+"""
+    read_field_info(sim_path)
+
+Reads the info on what fields are stored in the `particles` files.
+"""
+function read_field_info(sim_path)
+
+    # open first available file
+    file = select_first_file(sim_path, "fields")
+    # read all available output fields
     variables = adios_all_variable_names(file)
+    # field fields start with `f`
     out_fields = variables[startswith.(variables, "f")]
     for i = 1:length(out_fields)
         out_fields[i] = out_fields[i][2:end]
@@ -135,28 +190,43 @@ function read_field_info(sim_path; times=nothing, i_steps=nothing)
     return FieldInfo(out_fields)
 end
 
+"""
+    read_particle_info(sim_path)
+
+Reads the info on what fields are stored in the `particles` files.
+"""
 function read_particle_info(sim_path)
 
-    file = adios_open_serial(joinpath(sim_path, "particles", "particles.$(@sprintf("%08i", 1)).bp"), mode_readRandomAccess)
+    # open first available file
+    file = select_first_file(sim_path, "particles")
+    # read all available output fields
     variables = adios_all_variable_names(file)
+    # particle fields start with `p`
     out_fields = variables[startswith.(variables, "p")]
     for i = 1:length(out_fields)
         out_fields[i] = out_fields[i][2:end]
     end
-
-    # X1 is always written
+    # X1 is written for every species in all dimensions
     species = [parse(Int, sp[end]) for sp in out_fields[startswith.(out_fields, "X1")]]
-
     # get unique properties
     properties = unique([pr[1:end-2] for pr in out_fields])
 
     return ParticleInfo(species, properties)
 end
 
+
+"""
+    read_spectrum_info(sim_path)
+
+Reads the info on what fields are stored in the `spectrum` files.
+"""
 function read_spectrum_info(sim_path)
 
-    file = adios_open_serial(joinpath(sim_path, "spectrum", "spectrum.$(@sprintf("%08i", 1)).bp"), mode_readRandomAccess)
+    # open first available file
+    file = select_first_file(sim_path, "spectrum")
+    # read all available output fields
     variables = adios_all_variable_names(file)
+    # spectrum fields start with `s`
     out_fields = variables[startswith.(variables, "s")]
     for i = 1:length(out_fields)
         out_fields[i] = out_fields[i][2:end]
@@ -168,12 +238,22 @@ function read_spectrum_info(sim_path)
     return SpectrumInfo(species)
 end
 
+"""
+    read_run_settings(sim_path, subfolder)
+
+Read the general settings of a run from the first file in a subfolder.
+"""
 function read_run_settings(sim_path, subfolder)
-    file = adios_open_serial(joinpath(sim_path, subfolder, "$subfolder.$(@sprintf("%08i", 1)).bp"), mode_readRandomAccess)
+    
+    # open first available file
+    file = select_first_file(sim_path, subfolder)
+    # read run setting names
     attrs = adios_all_attribute_names(file)
+    # allocate storage dict
     settings = Dict{String, Any}()
+    # read all settings into the dict
     for attr in attrs
-        settings[attr] = adios_load(file, attr, 0)
+        settings[attr] = adios_attribute_data(file, attr)
     end
     close(file)
     return settings
@@ -186,7 +266,17 @@ Reads an overview of the simulation into an `EntityData` struct.
 """
 function EntityData(sim_path::String)
 
+    if !isdir(sim_path)
+        error("$sim_path not present!")
+    end
+
+    # check which output folders are present
     subfolders = readdir(sim_path)
+    # make sure to not try to read a plots folder
+    filter!(x -> x != "Plots", subfolders)
+    filter!(x -> x != "plots", subfolders)
+
+    # check if any folders are present
     if iszero(length(subfolders))
         error("No output subfolders found in $sim_path")
     end
@@ -194,24 +284,27 @@ function EntityData(sim_path::String)
     # reading output files and times
     times, i_steps = read_times(sim_path, subfolders[1], true)
 
+    # if fields are written, read their info
     field_info = nothing
     if "fields" in subfolders
         field_info = read_field_info(sim_path)
     end
+    
+    # if particles are written, read their info
     particle_info = nothing
     if "particles" in subfolders
         particle_info = read_particle_info(sim_path)
     end
+
+    # if spectra are written, read their info
     spectrum_info = nothing
     if "spectrum" in subfolders
         spectrum_info = read_spectrum_info(sim_path)
     end
 
     # read the settings of the simulation
-    runsettings = Dict("dummy" => 0) #read_run_settings(sim_path, subfolders[1])
+    runsettings = read_run_settings(sim_path, subfolders[1])
 
+    # construct an EntityData struct
     return EntityData(sim_path, i_steps, times, runsettings, field_info, particle_info, spectrum_info)
 end
-
-
-
--- src/IO/adios/fields.jl
+++ src/IO/adios/fields.jl
@@ -1,30 +1,33 @@
-function read_fields_reduced(run_info::EntityData, species::Integer, 
-                    properties::Vector{String}; 
-                    i, verbose)
+"""
+    read_fields_reduced(run_info::EntityData, field_names::Vector{String}; 
+                    i, reduction_function, verbose)
+
+Reads fields over an output range and computes a reduction based on `reduction_function`.
+"""
+function read_fields_reduced(run_info::EntityData, field_names::Vector{String}; 
+                    i, reduction_function, verbose)
 
     data = Dict()
     data["t"] = run_info.times[i]
     data["step"] = run_info.steps[i]
 
-    for prop in properties
-        field_name = "$(prop)_$(species)"
-        if !(field_name in ["$(pr)_$(sp)" for pr in run_info.particles.properties, sp in run_info.particles.species])
-            error("Property $prop for species $species not found in particle data.")
+    for field_name in field_names
+        if !(field_name in run_info.fields.fields)
+            error("Field $field_name not found in data.")
         end
-        data[prop] = Vector{Float64}(undef, length(i))
+        data[field_name] = Vector{Float64}(undef, length(i))
     end 
 
     for j in i
         # construct the file name
-        fi = joinpath(run_info.path, "particles", "particles.$(@sprintf("%08i", run_info.steps[j])).bp")
+        fi = joinpath(run_info.path, "fields", "fields.$(@sprintf("%08i", run_info.steps[j])).bp")
         # read the field
         file = adios_open_serial(fi, mode_readRandomAccess)
-        for prop in properties
-            field_name = "$(prop)_$(species)"
-            data[prop][j] = reduction_function(adios_load(file, "p$field_name", 0))
-        end    # remember to close the file
+        for field_name in field_names
+            data[field_name][j] = reduction_function(adios_load(file, "f$field_name", 0))
+        end    
+        # remember to close the file
         close(file)
-
     end
 
     return data
@@ -37,28 +40,33 @@ end
                     reduction_function=nothing,
                     verbose::Bool=false)
 
-Read fields with given `field_names`.
+Read fields with given `field_names` at a requested step `i_step`, time `t` or index `i`.
+If a `reduction_function` it is applied to the fields in the requested step, time or index range.
 """
 function read_field(run_info::EntityData, field_names::Vector{String}; 
                     i_step=nothing, t=nothing, i=nothing,
                     reduction_function=nothing,
                     verbose::Bool=false)
 
+    # check if all requested fields are present
+    for field_name in field_names
+        if !(field_name in run_info.fields.fields)
+            error("Field $field_name not found in data.")
+        end
+    end
+
+    # find relevant indices for requested times/steps
     i = find_idxs(run_info, i_step, t, i, reduction_function, verbose)
 
+    # compute reduced fields over requested timesteps
     if !isnothing(reduction_function)
-        return read_fields_reduced(run_info, species, properties; 
+        return read_fields_reduced(run_info, field_names; 
                     i=i,
                     reduction_function=reduction_function,
                     verbose=verbose)
     end
 
-    for j = 1:Nfields
-        if !(field_names[j] in run_info.fields.fields)
-            error("Field $field_name not found in data.")
-        end
-    end
-
+    # allocate storage dict
     data = Dict()
     data["t"] = run_info.times[i]
     data["step"] = run_info.steps[i]
@@ -93,10 +101,11 @@ end
                     reduction_function=nothing,
                     verbose::Bool=false)
 
-Same
+Reads a single field for an individual file. 
+If `reduction_function` is provied the reduced field is computed for the requested output or a range of outputs.
 """
 read_field(run_info::EntityData, field_name::String; 
-                    i_step=nothing, t=nothing, i=nothing,
-                    reduction_function=nothing,
-                    verbose::Bool=false) = read_field(run_info, [field_name];
-                    i_step, t, i, reduction_function, verbose)
+            i_step=nothing, t=nothing, i=nothing,
+            reduction_function=nothing,
+            verbose::Bool=false) = read_field(run_info, [field_name];
+            i_step, t, i, reduction_function, verbose)
