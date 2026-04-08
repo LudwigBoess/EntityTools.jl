21e9f4aaed920feab0b7403647e668f16e60f6e7
LudwigBoess <ludwig.boess@outlook.de>
2026-04-08 04:14:00 +0000

    minor fix

--- src/units/temperature.jl
+++ src/units/temperature.jl
@@ -11,13 +11,13 @@ const D = 0.936
 E(E_int, γ_bar, Γ) = (γ_bar - Γ) * Γ / ( 1 + γ_e(E_int) * (Γ^2 - 1) ) - E_int
 
 """
-    get_Temp(N::T, T00::T, V1::T, V2::T, V3::T) where T <: Real
+    get_Temp(N::T, T00::T, V1::T, V2::T, V3::T; adiabatic_index::Real=5/3) where T <: Real
 
 Compute the temperature based on Sridhar et al. (2021, https://arxiv.org/pdf/2107.00263), Appendix C.
 It uses the number density `N`, total energy `T00`, and velocity components `V1`, `V2`, and `V3`.
 The function uses a root-finding method to solve for the internal energy and then converts it to temperature using the relation T = (2/3) * E_int.
 """
-function get_Temp(N::T, T00::T, V1::T, V2::T, V3::T) where T <: Real
+function get_Temp(N::T, T00::T, V1::T, V2::T, V3::T; adiabatic_index::Real=5/3) where T <: Real
 
     v_bulk = V1^2 + V2^2 + V3^2
     γ_bar = T00 / N
@@ -32,6 +32,6 @@ function get_Temp(N::T, T00::T, V1::T, V2::T, V3::T) where T <: Real
     end
 
     # internal energy to temperature conversion
-    Temp *= 2/3
+    Temp *= (adiabatic_index - 1)
     return Temp
 end
